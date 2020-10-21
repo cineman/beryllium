759ac33365a362dc3cfc9ccd7374cb4b68797bba
Mario Döring <deluna.yy@gmail.com>
2020-10-21 14:09:08 +0200

    Added mutex system

---
+++ src/Mutex.php
@@ -0,0 +1,113 @@
+<?php 
+
+namespace Beryllium;
+
+use Beryllium\Driver\DriverInterface;
+
+use Beryllium\Exception\LockedMutexException;
+
+class Mutex
+{
+    /**
+     * Driver instance
+     *
+     * @var DriverInterface
+     */
+    private DriverInterface $driver;
+
+    /**
+     * Mutex key
+     *
+     * @var string
+     */
+    private string $lockkey;
+
+    /**
+     * The mutex current token that ensures that only 
+     * this instance can unlock a lock.
+     *
+     * @var string
+     */
+    private string $token;
+
+    /**
+     * Error codes
+     */
+    const ERROR_ALREADY_LOCKED = 5;
+    const ERROR_UNLOCK_FAILURE = 10;
+
+    /**
+     * Constructor
+     *
+     * @param DriverInterface               $driver The beryllium driver.
+     * @param string                        $lockkey
+     * @return void 
+     */
+    public function __construct(DriverInterface $driver, string $lockkey)
+    {
+        $this->driver = $driver;
+        $this->lockkey = $lockkey;
+    }
+
+    /**
+     * Lock the mutex
+     *
+     * @throws LockedMutexException
+     * 
+     * @param int               $tll Max time to live in seconds.
+     * @return void
+     */
+    public function lock(int $ttl = 10)
+    {
+        // generate a token
+        $this->token = uniqid();
+
+        // try to lock on the driver
+        if (!$this->driver->lock($this->lockkey, $this->token, $ttl)) {
+            throw new LockedMutexException("The mutex ($this->lockkey) is already locked.", static::ERROR_ALREADY_LOCKED);
+        }
+    }
+
+    /**
+     * Lock the mutex
+     *
+     * @throws LockedMutexException
+     * 
+     * @return void
+     */
+    public function unlock()
+    {
+        // try to lock on the driver
+        if (!$this->driver->unlock($this->lockkey, $this->token)) {
+            throw new LockedMutexException("The mutex ($this->lockkey) could not be unlocked, either its been locked by another instance or it does not exist.", static::ERROR_UNLOCK_FAILURE);
+        }
+    }
+
+    /**
+     * Runs the given callback in a mutex locked enclosure.
+     * Catches any error that might occour and unlocks the mutex and rethrows the error / exception.
+     *
+     * @throws \Error
+     * @throws \Exception
+     *
+     * @param callable              $callback 
+     * @param int                   $ttl
+     * @return void
+     */
+    public function safeExec(callable $callback, int $ttl = 10)
+    {
+        $this->lock($ttl);
+
+        try {
+            $callback($this->token);
+        } catch(\Exception $e) {
+            $this->unlock();
+            throw $e;
+        } catch(\Error $e) {
+            $this->unlock();
+            throw $e;
+        }
+
+        $this->unlock();
+    }
+}
