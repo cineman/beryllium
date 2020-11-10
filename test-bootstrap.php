fc4a063a7fc3839b2e4d2729db58244e0efebd59
Mario Döring <deluna.yy@gmail.com>
2020-11-10 17:14:08 +0100

    Moved and chacnged

--- test-bootstrap.php
+++ test-bootstrap.php
@@ -17,6 +17,8 @@ use Beryllium\Driver\RedisDriver;
  */
 if (!defined('BERYLLIUM_REDIS_HOST')) define('BERYLLIUM_REDIS_HOST', '127.0.0.1');
 if (!defined('BERYLLIUM_REDIS_PORT')) define('BERYLLIUM_REDIS_PORT', 6379);
+if (!defined('BERYLLIUM_IDLE_WAIT')) define('BERYLLIUM_IDLE_WAIT', 10000); // 10ms
+if (!defined('BERYLLIUM_MAX_WORKERS')) define('BERYLLIUM_MAX_WORKERS', 8);
 
 /**
  * Create a queue instance with the default configuration
@@ -24,4 +26,26 @@ if (!defined('BERYLLIUM_REDIS_PORT')) define('BERYLLIUM_REDIS_PORT', 6379);
 $redis = new Redis();
 $redis->pconnect(BERYLLIUM_REDIS_HOST, BERYLLIUM_REDIS_PORT);
 
-return new Queue(new RedisDriver($redis));
+/**
+ * Demo function to build proof of work
+ */
+function proof_of_work(string $key, int $difficulty = 1) : int
+{
+    $it = 0;
+    $p = str_repeat('0', $difficulty);
+
+    do {
+        $hash = md5($key . $it);
+        $it++;
+    } while(substr($hash, 0, $difficulty) !== $p);
+
+    return $it;
+}
+
+/**
+ * Return resources
+ */
+return [
+    new Queue(new RedisDriver($redis)),
+    $redis
+];
